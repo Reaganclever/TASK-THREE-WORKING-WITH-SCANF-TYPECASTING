2f24fed6bf9fbe53ef4f380e2243457b7a007b3d
reaganclever <kasulereagan86@gmail.com>
2025-06-12 13:59:37 +0300

    TTWWSTC3-1 COMMIT#2 ON BRANCH#1

--- AverageSalesCalculation.c
+++ AverageSalesCalculation.c
@@ -4,10 +4,10 @@
 double calculateAverageSales(double totalsales , int days){
 
 double averageSales = totalsales * (double)days;
-printf("\nNEW TOTAL SALES:\t%.2lf\n" , totalsales);
-printf("NEW NO OF DAYS:\t%d\n" , days);
+printf("\nNEWEST TOTAL SALES:\t%.2lf\n" , totalsales);
+printf("NEWEST NO OF DAYS:\t%d\n" , days);
 printf("----------\n");
-printf("NEW AVERAGE SALES:\t%.2lf\n" , averageSales);
+printf("NEWEST AVERAGE SALES:\t%.2lf\n" , averageSales);
 
 return averageSales;
 
