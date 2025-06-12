f3cb04dca6e8979e8e4ebede7cf199e0c026a460
reaganclever <kasulereagan86@gmail.com>
2025-06-12 14:03:49 +0300

    TTWWSTC3-6 COMMIT#1 ON BRANCH#2

--- CommissionCalculation.c
+++ CommissionCalculation.c
@@ -6,10 +6,10 @@ double computeCommissionCalculation(float salesVolume , double rate){
 
 double commission = (double)salesVolume * rate;
 
-printf("\nSALES.VOLUME:\t%.2f\n" , salesVolume);
-printf("COMMISSION.RATE:%.2lf\n" , rate);
+printf("\nNEW SALES.VOLUME:\t%.2f\n" , salesVolume);
+printf("NEW COMMISSION.RATE:%.2lf\n" , rate);
 printf("---------------\n");
-printf("COMMISSION.AMOUNT:%.2lf\n" , commission);
+printf("NEW COMMISSION.AMOUNT:%.2lf\n" , commission);
 return commission;
 
 }
