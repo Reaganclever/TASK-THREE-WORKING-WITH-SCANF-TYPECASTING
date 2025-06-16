c0e9c3c0c3aff1af28e4ea7a8d7dfed66cb4a89b
reaganclever <kasulereagan86@gmail.com>
2025-06-16 18:14:44 +0300

    TTWWSTC3-21 COMMIT#1 ON BRANCH#5

--- TotalRevenue.c
+++ TotalRevenue.c
@@ -5,11 +5,11 @@
 double calculateTotalRevenue(double customer1 , float customer2 , double customer3){
 
 double totalRevenue = customer1 + (double)customer2 + customer3;
-printf("CUSTOMER1:\t%.2lf\n" , customer1);
-printf("CUSTOMER2:\t%.2f\n" , customer2);
-printf("CUSTOMER3:\t%.2lf\n" , customer3);
+printf("NEW CUSTOMER1:\t%.2lf\n" , customer1);
+printf("CUREENT CUSTOMER2:\t%.2f\n" , customer2);
+printf(" NEWCUSTOMER3:\t%.2lf\n" , customer3);
 printf("------------------------\n");
-printf("TOTAL REVENUE:\t%.2lf\n" , totalRevenue);
+printf("NEW TOTAL REVENUE:\t%.2lf\n" , totalRevenue);
 return totalRevenue;
 
 }
