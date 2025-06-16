1fd5835f5639b8cbadf70a54549f7aedad60aa04
reaganclever <kasulereagan86@gmail.com>
2025-06-16 18:05:20 +0300

    TTWWSTC3-11 COMMIT#1 ON BRANCH#3

--- StraightLineDepreciation.c
+++ StraightLineDepreciation.c
@@ -4,11 +4,11 @@
 void calculatestraightLineDepreciation(double assetCost , float residualValue , double UsefulLife){
 
 double depreciation = (assetCost - residualValue) / UsefulLife;
-printf("ASSET COST:\t%.2lf\n" , assetCost);
-printf("RESIDUAL VALUE:\t%.2f\n" , residualValue);
-printf("USEFUL LIFE:\t%.2lf\n" , UsefulLife);
+printf("NEW ASSET COST:\t%.2lf\n" , assetCost);
+printf("NEW RESIDUAL VALUE:\t%.2f\n" , residualValue);
+printf("NEW USEFUL LIFE:\t%.2lf\n" , UsefulLife);
 printf("-----------------------\n");
-printf("ANNUAL DEPRECIATION:\tUGX%.2lf\n" , depreciation);
+printf("NEW ANNUAL DEPRECIATION:\tUGX%.2lf\n" , depreciation);
 }
 
 
