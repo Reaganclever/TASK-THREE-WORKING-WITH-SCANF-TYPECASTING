c4bcd5e5ed9b7b5512e7b14ede067e51a2efe72e
reaganclever <kasulereagan86@gmail.com>
2025-06-16 18:11:06 +0300

    TTWWSTC3-16 COMMIT#2 ON BRANCH#4

--- AnnualSalaryCalculation.c
+++ AnnualSalaryCalculation.c
@@ -3,11 +3,11 @@
 
 double computeAnnualSalary(double hourlyWage , float hoursPerWeek , double weeksPerYear){
 double annualSalary = hourlyWage * (double)hoursPerWeek * weeksPerYear;
-printf("NEW HOURLY WAGE:\t%.2lf\n" , hourlyWage);
-printf("NEW HOURS PER WEEK:\t%.2f\n" , hoursPerWeek);
-printf("NEW WEEKS PER YEAR:\t%.2lf\n" , weeksPerYear);
+printf("NEWEST HOURLY WAGE:\t%.2lf\n" , hourlyWage);
+printf("UPDATED HOURS PER WEEK:\t%.2f\n" , hoursPerWeek);
+printf("CURRENT WEEKS PER YEAR:\t%.2lf\n" , weeksPerYear);
 printf("------------------------\n");
-printf("NEW ANNUAL SALARY:\t%.2lf\n" , annualSalary);
+printf("FRESH ANNUAL SALARY:\t%.2lf\n" , annualSalary);
 
 }
 
